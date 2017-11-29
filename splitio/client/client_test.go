305bb911b7cefe5059affdbb393a532fb35760f2
Martin Redolatti <martinredolatti@gmail.com>
2017-11-29 16:06:34 -0300

    add localhost operation mode tests

--- splitio/client/client_test.go
+++ splitio/client/client_test.go
@@ -5,6 +5,8 @@ import (
 	"github.com/splitio/go-client/splitio/engine/evaluator"
 	"github.com/splitio/go-client/splitio/storage/mutexmap"
 	"github.com/splitio/go-toolkit/logging"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -89,4 +91,38 @@ func TestTreatments(t *testing.T) {
 		t.Error("Incorrect result for \"notFeature\"")
 	}
 
+}
+
+func TestLocalhostMode(t *testing.T) {
+	file, err := ioutil.TempFile("", "splitio_tests")
+	if err != nil {
+		t.Error("Couldn't create temporary file for localhost client tests: ", err)
+		return
+	}
+
+	file.Write([]byte("feature1 on\n"))
+	file.Write([]byte("feature2 off\n"))
+	file.Sync()
+
+	sdkConf := conf.Default()
+	sdkConf.SplitFile = file.Name()
+	factory, _ := NewSplitFactory("localhost", sdkConf)
+	client := factory.Client()
+
+	if client.cfg.OperationMode != "localhost" {
+		t.Error("Localhost operation mode should be set when received apikey is 'localhost'")
+	}
+
+	feature1 := client.Treatment("asd", "feature1", nil)
+	if feature1 != "on" {
+		t.Error("Feature1 retrieved incorrectly")
+	}
+
+	feature2 := client.Treatment("asd", "feature2", nil)
+	if feature2 != "off" {
+		t.Error("Feature2 retrieved incorrectly")
+	}
+
+	file.Close()
+	os.Remove(file.Name())
 }

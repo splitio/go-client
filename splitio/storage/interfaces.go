b3231b520b4531097472f8030e6baec768d20dd5
Martin Redolatti <martinredolatti@gmail.com>
2017-11-29 19:16:32 -0300

    update documentation for interfaces

--- splitio/storage/interfaces.go
+++ splitio/storage/interfaces.go
@@ -5,25 +5,19 @@ import (
 	"github.com/splitio/go-toolkit/datastructures/set"
 )
 
-// SplitStorageProducer should be implemented by structs wanting to write splits to the storage
+// SplitStorageProducer should be implemented by structs that offer writing splits in storage
 type SplitStorageProducer interface {
 	PutMany(splits []dtos.SplitDTO, changeNumber int64)
 	Remove(splitname string)
 }
 
-// SplitStorageConsumer should be implemented by structs wanting to read splits from storage
+// SplitStorageConsumer should be implemented by structs that offer reading splits from storage
 type SplitStorageConsumer interface {
 	Get(splitName string) *dtos.SplitDTO
 	Till() int64
 	SplitNames() []string
 	SegmentNames() *set.ThreadUnsafeSet
 	GetAll() []dtos.SplitDTO
-}
-
-// SplitStorage is a wrapper for consumer & producer interfaces
-type SplitStorage interface {
-	SplitStorageProducer
-	SplitStorageConsumer
 }
 
 // SegmentStorageProducer interface should be implemented by all structs that offer writing segments
@@ -38,12 +32,6 @@ type SegmentStorageConsumer interface {
 	Till(segmentName string) int64
 }
 
-// SegmentStorage wraps consumer and producer interfaces
-type SegmentStorage interface {
-	SegmentStorageProducer
-	SegmentStorageConsumer
-}
-
 // ImpressionStorageProducer interface should be impemented by structs that accept incoming impressions
 type ImpressionStorageProducer interface {
 	Put(feature string, impression *dtos.ImpressionDTO)
@@ -54,25 +42,41 @@ type ImpressionStorageConsumer interface {
 	PopAll() []dtos.ImpressionsDTO
 }
 
-// ImpressionStorage Interface wraps consumer & producer interfaces
-type ImpressionStorage interface {
-	ImpressionStorageConsumer
-	ImpressionStorageProducer
-}
-
+// MetricsStorageProducer interface should be impemented by structs that accept incoming metrics
 type MetricsStorageProducer interface {
 	PutGauge(key string, gauge float64)
 	IncLatency(metricName string, index int)
 	IncCounter(key string)
 }
 
+// MetricsStorageConsumer interface should be implemented by structs that offer popping metrics
 type MetricsStorageConsumer interface {
 	PopGauges() []dtos.GaugeDTO
 	PopLatencies() []dtos.LatenciesDTO
 	PopCounters() []dtos.CounterDTO
 }
 
-// MetricsStorage Interface should be implemented by all metrics storage storage forms
+// --- Wide Interfaces
+
+// SplitStorage wraps consumer & producer interfaces
+type SplitStorage interface {
+	SplitStorageProducer
+	SplitStorageConsumer
+}
+
+// SegmentStorage wraps consumer and producer interfaces
+type SegmentStorage interface {
+	SegmentStorageProducer
+	SegmentStorageConsumer
+}
+
+// ImpressionStorage wraps consumer & producer interfaces
+type ImpressionStorage interface {
+	ImpressionStorageConsumer
+	ImpressionStorageProducer
+}
+
+// MetricsStorage wraps consumer and producer interfaces
 type MetricsStorage interface {
 	MetricsStorageConsumer
 	MetricsStorageProducer

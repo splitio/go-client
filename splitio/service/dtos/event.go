b010a208e238f4181f854437e5d2aa30dcc613a8
Sebastian Arrubia <sarrubia@gmail.com>
2018-01-25 17:33:48 -0300

    Adding Events DTO

---
+++ splitio/service/dtos/event.go
@@ -0,0 +1,33 @@
+package dtos
+
+//
+// Events DTOs
+//
+
+// RedisStoredMachineMetadataDTO alias of QueueStoredMachineMetadataDTO
+type RedisStoredMachineMetadataDTO QueueStoredMachineMetadataDTO
+
+// RedisStoredEventDTO alias of QueueStoredEventDTO
+type RedisStoredEventDTO QueueStoredEventDTO
+
+// EventDTO struct mapping events json
+type EventDTO struct {
+	Key             string   `json:"key"`
+	TrafficTypeName string   `json:"trafficTypeName"`
+	EventTypeID     string   `json:"eventTypeId"`
+	Value           *float64 `json:"value"`
+	Timestamp       int64    `json:"timestamp"`
+}
+
+// QueueStoredMachineMetadataDTO maps sdk version, machine IP and machine name
+type QueueStoredMachineMetadataDTO struct {
+	SDKVersion  string `json:"s"`
+	MachineIP   string `json:"i"`
+	MachineName string `json:"n"`
+}
+
+// QueueStoredEventDTO maps the stored JSON object in redis by SDKs
+type QueueStoredEventDTO struct {
+	Metadata QueueStoredMachineMetadataDTO `json:"m"`
+	Event    EventDTO                      `json:"e"`
+}
